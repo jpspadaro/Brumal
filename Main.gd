c335ab3930e3e0779ad2237d2b9e01f5fec4ccec
Jason Spadaro <jpspadaro@gmail.com>
2021-11-21 13:50:32 -0500

    Expanded main scene. Basic player controls. Basic HUD.

---
+++ Main.gd
@@ -0,0 +1,18 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+func _unhandled_input(event):
+	if event.is_action_pressed("quit"):
+		get_tree().quit()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
